bf5e1d6be2091c22fea5b7a255063dc2a8b5b770
Susanna Law <susannalawuk@gmail.com>
2026-06-04 09:33:07 +0100

    V5 - exercise screen is fixed now

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,7 @@ void setup() {
   CircuitPlayground.begin();
   Wire.begin(); 
   u8g2.begin(); 
+  randomSeed(analogRead(A6)); // 
 }
 
 void loop() {
@@ -87,7 +88,7 @@ void findMaxForce() { // Find the maximum force through the sensor
     for (int i = 0; i < numSensors; i++) { // For each sensor
       currentFinger = i + 1;
       initialiseScreen(nextFinger);
-      delay(2000);
+      delay(4000);
 
       unsigned long fingerStart = millis(); // resets to "now" for each finger
       
@@ -118,14 +119,16 @@ void findMaxForce() { // Find the maximum force through the sensor
     }
   }
 
-  delay(2000);
 
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB08_tr);
   u8g2.drawStr(5, 20, "Press LEFT Button");
   u8g2.drawStr(5, 40, "to continue:");
   u8g2.sendBuffer();
-}
+
+  while (!CircuitPlayground.leftButton());         // wait for press
+  while (CircuitPlayground.leftButton());          // wait for release
+} 
 
 
 void findForceThresholdValue(){ // Finding the threshold value
@@ -188,7 +191,7 @@ void showRetryScreen() {
   u8g2.setFont(u8g2_font_ncenB08_tr);
   u8g2.drawStr(5, 20, "Please retry:");
   u8g2.sendBuffer();
-  delay(1000);
+  delay(4000);
 }
 
 // function for exercising the fingers
@@ -247,7 +250,17 @@ void areTheyTouchingThePad (){
 
   }
   initialiseScreen(completionScreen);
-  delay(2000);
+  delay(4000);
+
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.drawStr(5, 20, "Press LEFT Button");
+  u8g2.drawStr(5, 40, "to continue:");
+  u8g2.sendBuffer();
+  delay(4000);
+
+  while (!CircuitPlayground.leftButton());         // wait for press
+  while (CircuitPlayground.leftButton());          // wait for release
 }
 
 void programRuntime() { // what the program runs
@@ -263,7 +276,7 @@ void programRuntime() { // what the program runs
       initialiseScreen(calibratingScreen); //to calibrate the force
 
 
-      delay(2000);
+      delay(4000);
       // Calibrates the finger force, iterates through each of the fingers and calculates the maximum force
         findMaxForce();
 
@@ -294,6 +307,5 @@ void programRuntime() { // what the program runs
   lastLeftButtonPressed = leftButtonPressed; // state machine control
   delay(10); 
 }
-
 
 
